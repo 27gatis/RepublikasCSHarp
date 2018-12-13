9d449971f3f0cbe453ce31fb3288ffe4abf978ad
27gatis <gatis.arakcejevs@va.lv>
2018-12-13 19:47:56 +0200

    Izveidots projekts un pirmais uzdevums

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ka jus sauc?");
+            String vards = Console.ReadLine();
+            Console.WriteLine("Sveiki, " + vards);
+
+
+            // int, double, String, bool ...
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
