a0656333492eb10b9a37fabbac7227ab25685c23
27gatis <gatis.arakcejevs@va.lv>
2018-12-17 21:31:34 +0200

    Day3
    VIendimensiju masivi - uzdevumi

---
+++ Day3/Day3/Piemeri.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Piemeri
+    {
+        public void MasivaPirmaisPiemers()
+        {
+            //int skaitlis = 4;
+
+            String[] zvaigzne = new String[4];
+            //masivs[0] = 1;
+            //masivs[1] = 4;
+            //masivs[2] = 5;
+            // int garums = masivs.Length;
+
+            for (int i = 0; i < zvaigzne.Length; i++)
+            {
+                if (i == 0)
+                {
+                    zvaigzne[i] = "*";
+                }
+                else
+                {
+                    zvaigzne[i] = zvaigzne[i - 1] + "*";
+                }
+
+                Console.WriteLine(zvaigzne[i]);
+            }
+
+
+            for(int i = zvaigzne.Length-1; i>=0; i--)
+            {
+                Console.WriteLine(zvaigzne[i]);
+            }
+
+            //// MASIVA UZDEVUMS
+            ///ar 4.punktiem
+           
+        }
+        public void MasivaIevade()
+        {
+            Console.WriteLine(" Ievadiet masiva garumu");
+            int garums = Convert.ToInt16(Console.ReadLine());
+
+            String[] masivs = new String[garums]; // 2.punkts
+
+            for (int i = 0; i < masivs.Length; i++)
+            {
+                Console.WriteLine("Ievadiet masiva" + i + "." + " elementu");
+                masivs[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("------------------");
+
+            for (int i = 0; i < masivs.Length; i++ )
+            {
+                Console.WriteLine(masivs[i]);
+            }
+        }
+            
+    }
+
+
+       
+
+    }
---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadiet vertibu");
+            Console.ReadLine();
+            Piemeri piem = new Piemeri();
+            piem.MasivaPirmaisPiemers();
+            Console.ReadLine();
+        }
+
+        
+    }
+}
