52b2fb7f2a01e2b2d739f8511d8b4b954c83ad3a
27gatis <gatis.arakcejevs@va.lv>
2019-01-06 00:26:50 +0200

    Pievienota dzest vienu, dzest visus, rakstit  jauna faila

--- MajasdarbsLietotajuLasisanaRakstisana/MajasdarbsLietotajuLasisanaRakstisana/Uzdevums.cs
+++ MajasdarbsLietotajuLasisanaRakstisana/MajasdarbsLietotajuLasisanaRakstisana/Uzdevums.cs
@@ -17,7 +17,7 @@ namespace MajasdarbsLietotajuLasisanaRakstisana
         //0.Iziet
 
         private List<String> lietotaji = new List<String>();
-        private List<int> lietotajuNumuri = new List<int>();
+        private List<String> lietotajuNumuri = new List<String>();
 
         public void Menu()
         {
@@ -29,8 +29,10 @@ namespace MajasdarbsLietotajuLasisanaRakstisana
                 Console.WriteLine("1.- Izvadit visus lietotajus");
                 Console.WriteLine("2.- Izvadit konkretu lietotaju");
                 Console.WriteLine("3.- Pievienot sarakstam");
-                Console.WriteLine("4.- Random lietotajs");
+                Console.WriteLine("4.- Izdzest lietotaju");
+                Console.WriteLine("9.- Random lietotajs");
                 Console.WriteLine("5.- iztirit failu");
+                Console.WriteLine("6.- Ierakstit sarakstu faila");
                 Console.WriteLine("0. - iziet");
 
                 choice = Console.ReadLine();
@@ -43,13 +45,20 @@ namespace MajasdarbsLietotajuLasisanaRakstisana
                         IzvaditKonkretu();// strada, bet neizmanto numuru failu
                         break;
                     case "3":
-                        //Pievienot();
+                        Pievienot(); // numuri dublejas
                         break;
-                    case "4":
+                    case "9":
                         //RandomJoks();
                         break;
+                    case "4":
+                        IzdzestLietotaju(); //NESTRADA
+                        break;
                     case "5":
-                        //Tirit();
+                        DzestVisusLietotajus(); //OK
+                        break;
+                    case "6":
+                        RakstisanaJaunaFaila fails = new RakstisanaJaunaFaila(); //OK
+                        fails.Rakstit(lietotaji);
                         break;
                     case "0":
                         break;
@@ -74,7 +83,7 @@ namespace MajasdarbsLietotajuLasisanaRakstisana
         private void IzvaditKonkretu()
         {
             RefreshLietotaji();
-            Console.WriteLine("Ievadiet indeksu!");
+            Console.WriteLine("Ievadiet indeksu:");
             int lietotajaNr = Convert.ToInt32(Console.ReadLine());
 
             if (lietotajaNr > 0 && lietotajaNr <= lietotaji.Count)
@@ -88,6 +97,88 @@ namespace MajasdarbsLietotajuLasisanaRakstisana
 
         }
 
+
+        private void Pievienot()
+        {
+            RefreshLietotaji();
+            Console.WriteLine("Ievadi jauno lietotaju:");
+            String teksts = Console.ReadLine();
+            //int numurs = Convert.ToInt32(Console.ReadLine());
+
+            if (teksts != "")
+            {
+                lietotaji.Add(teksts);
+                //lietotajuNumuri.Add(teksts); // raksta otra(ID) faila vardus
+            }
+            else
+            {
+                Console.WriteLine("Nepareiza Ievade!");
+                return;
+            }
+
+
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"D:\VisualPiemeri\lietotaji.txt"))
+            {
+                foreach (String lietotajs in lietotaji)
+                {
+                    file.WriteLine(lietotajs); //rada ciparu divreiz
+
+                }
+
+            }
+
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"D:\VisualPiemeri\lietotajunumuri.txt"))
+            {
+                foreach (String lietotajanumurs in lietotajuNumuri)
+                {
+                    file.WriteLine(lietotajanumurs); //rakstisana 2.faila - raksta tikai vardu?????
+
+                }
+
+            }
+
+        }
+
+
+
+        private void IzdzestLietotaju()
+        {
+            Console.WriteLine("Ievadiet lietotaja ID:");
+            //int id = Convert.ToInt16(Console.ReadLine());
+            string id = Console.ReadLine();
+
+            bool atrasts = false;
+            for (int i = 0; i < lietotaji.Count; i++)
+            {
+                if (lietotaji.Contains(id))
+                {
+                    //Console.WriteLine
+                    lietotaji.RemoveAt(i);
+                    lietotajuNumuri.RemoveAt(i);
+                    Console.WriteLine("Ieraksts dzests");
+                    atrasts = true;
+                    break;
+                }
+            }
+
+            if (atrasts != true)
+            {
+                Console.WriteLine("ID netika atrast");
+            }
+        }
+
+        
+        private void DzestVisusLietotajus()
+        {
+            lietotaji.Clear();
+            using (System.IO.StreamWriter file =
+            new System.IO.StreamWriter(@"D:\VisualPiemeri\lietotaji.txt"))
+            {
+                file.Write("");
+            }
+        }
 
 
         private void RefreshLietotaji()
@@ -111,11 +202,11 @@ namespace MajasdarbsLietotajuLasisanaRakstisana
             {
                 if (lines2[i] != "")
                 {
-                    lietotajuNumuri.Add(Convert.ToInt32(lines2[i]));
+                    lietotajuNumuri.Add(lines2[i]);
                 }
 
             }
-
+            
 
         }
 
